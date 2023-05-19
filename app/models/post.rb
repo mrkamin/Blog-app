1a5deb2390cff450a5513b64251f745863be56c5
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-05-20 02:31:24 +0500

    Processes data in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  has_many :likes, foreign_key: :post_id, dependent: :delete_all
+  has_many :comments, foreign_key: :post_id, dependent: :delete_all
+
+  after_save :update_post_counter
+
+  def latest_five_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def update_post_counter
+    author.update(posts_counter: author.posts.all.length)
+  end
+
+  private :update_post_counter
 end
