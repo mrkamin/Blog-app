b90bbac65b63fe9a39e620609eedcc776dd4cd9f
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-05-25 19:27:00 +0500

    Add Review Feedback

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,43 +1,37 @@
 require 'rails_helper'
-RSpec.describe 'Posts', type: :request do
-  user = User.new(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+RSpec.describe 'Users', type: :request do
+  user = User.new(name: 'Tom',
+                  photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                  bio: 'Anything test',
                   posts_counter: 0)
   user.save
-  post = Post.new(
-    title: 'Title 1',
-    text: 'Text 1',
-    author: user,
-    comments_counter: 0,
-    likes_counter: 0
-  )
-  post.save
 
-  describe 'Post GET /index' do
-    it 'returns post http success' do
-      get "/users/#{user.id}/posts/"
+  describe 'User GET /index' do
+    it 'returns user http success' do
+      get '/users/'
       expect(response).to have_http_status(200)
     end
-    it 'renders post template' do
-      get "/users/#{user.id}/posts/"
+    it 'renders user template' do
+      get '/users/'
       expect(response).to render_template(:index)
     end
-    it 'post respond body with correct place holder' do
-      get "/users/#{user.id}/posts/"
-      expect(response.body).to include('Posts attached to a user Page')
+    it 'user respond body with correct place holder' do
+      get '/users/'
+      expect(response.body).to include('Users Page')
     end
   end
-  describe 'Post GET /show' do
-    it 'return success for detail post' do
-      get "/users/#{user.id}/posts/#{post.id}"
+  describe 'User GET /show' do
+    it 'return success for detail user' do
+      get "/users/#{user.id}"
       expect(response).to have_http_status(200)
     end
-    it 'renders post detail template' do
-      get "/users/#{user.id}/posts/#{post.id}"
+    it 'renders user detail template' do
+      get "/users/#{user.id}"
       expect(response).to render_template(:show)
     end
-    it 'post detail respond body with correct place holder' do
-      get "/users/#{user.id}/posts/#{post.id}"
-      expect(response.body).to include('Here is a particular posts of index 4 for a user')
+    it 'user detail respond body with correct place holder' do
+      get "/users/#{user.id}"
+      expect(response.body).to include('Here is user index 1 page')
     end
   end
 end
