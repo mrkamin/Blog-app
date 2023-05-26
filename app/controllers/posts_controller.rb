acee763affd2d4e2dba196db46c23c9baa290265
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-05-27 01:49:52 +0500

    Adding-forms-to-Blog-app

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,26 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+    @showcurrentuser = current_user
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      flash[:success] = 'Post has benn created successfully'
+      redirect_to user_post_path(current_user, @post)
+    else
+      flash.now[:error] = 'Error: Post was not created !!'
+      render :new
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
+  private :post_params
 end
