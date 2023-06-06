1548475ef7cbaa15d8ff6c0d793fb1412c729b0f
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-06-06 19:03:40 +0500

    Conflict Resolved

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,22 +3,14 @@ RSpec.describe 'Posts', type: :request do
   user = User.new(name: 'Tom Hinns', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
                   posts_counter: 0)
   user.save
-  post = Post.new(
-    title: 'Title 1',
-    text: 'Text 1',
-    author: user,
-    comments_counter: 0,
-    likes_counter: 0
-  )
-  post.save
 
-  describe 'Post GET /index' do
-    it 'returns post http success' do
-      get "/users/#{user.id}/posts/"
+  describe 'User GET /index' do
+    it 'returns user http success' do
+      get '/users/'
       expect(response).to have_http_status(200)
     end
-    it 'renders post template' do
-      get "/users/#{user.id}/posts/"
+    it 'renders user template' do
+      get '/users/'
       expect(response).to render_template(:index)
     end
     it 'post respond body with correct place holder' do
@@ -26,13 +18,13 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Tom Hinns')
     end
   end
-  describe 'Post GET /show' do
-    it 'return success for detail post' do
-      get "/users/#{user.id}/posts/#{post.id}"
+  describe 'User GET /show' do
+    it 'return success for detail user' do
+      get "/users/#{user.id}"
       expect(response).to have_http_status(200)
     end
-    it 'renders post detail template' do
-      get "/users/#{user.id}/posts/#{post.id}"
+    it 'renders user detail template' do
+      get "/users/#{user.id}"
       expect(response).to render_template(:show)
     end
     it 'post detail respond body with correct place holder' do
